61eaa324d98bb20a0709a1c159e24a403f46537d
Asuka Lee <asuka.li@emory.edu>
2023-02-24 13:54:34 -0500

    search bar update

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ var userRouter = require('./user')
 var projectRouter = require('./projects')
 var modelRouter = require('./models')
 var resultRouter = require('./results')
+var searchRouter = require('./search')
 
 router.use('/user', userRouter)
 router.use('/projects', projectRouter)
 router.use('/models', modelRouter)
 router.use('/results', resultRouter)
+router.use('/search', searchRouter)
 
 module.exports = router
---
+++ routes/search.js
@@ -0,0 +1,41 @@
+const express = require("express");
+var router = express.Router();
+const db = require("../models");
+const { Op } = require("sequelize");
+
+router.post("/", async (req, res) => {
+  let { title } = req.body;
+  console.log(title);
+
+  let searcharr = {};
+  try {
+    const projects = await db.Project.findAll({
+      where: {
+        Title: {
+          [Op.like]: `%${title}%`,
+        },
+      },
+      attributes: ["Id", "Title"],
+    });
+    searcharr["projects"] = projects;
+  } catch (err) {
+    console.error(err);
+  }
+  try {
+    const models = await db.Model.findAll({
+      where: {
+        Name: {
+          [Op.like]: `%${title}%`,
+        },
+      },
+      attributes: ["Id", "Name"],
+    });
+    searcharr["models"] = models;
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.send(searcharr);
+});
+
+module.exports = router;
